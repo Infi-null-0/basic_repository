e5582e8c097bd535ee9fc3fdad025a9e5fc85a86
infi-null-0-local <119103652+Infi-null-0@users.noreply.github.com>
2022-11-27 20:40:13 +0530

    Update c_pogram_1.c
    PROGRAM FOR GETTING AND PRINTING TWO NUMBERS

---
+++ c_pogram_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int num1, num2;
+    printf("PROGRAM TO PRINT TWO NUMBERS\n\nEnter the two numbers :");
+    scanf("%d%d", &num1, &num2);
+    printf("\nThe number 1 is : %d\nThe number 2 is : %d\n\nEND OF PROGRAM", num1, num2);
+}
