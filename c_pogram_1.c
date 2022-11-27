21a2413dff8ca5df533272a26e4a3b4ad857af5c
infi-null-0-local <119103652+Infi-null-0@users.noreply.github.com>
2022-11-27 20:47:59 +0530

    Update c_pogram_1.c
    added an update to subtract numbers

--- c_pogram_1.c
+++ c_pogram_1.c
@@ -5,5 +5,5 @@ void main()
     int num1, num2;
     printf("PROGRAM TO PRINT TWO NUMBERS\n\nEnter the two numbers :");
     scanf("%d%d", &num1, &num2);
-    printf("\nThe number 1 is : %d\nThe number 2 is : %d\n\nEND OF PROGRAM", num1, num2);
+    printf("\nThe number 1 is : %d\nThe number 2 is : %d\n\nTHE DIFFERENCE IN BOTH THE NUMBERS ARE : %d\n\nEND OF PROGRAM", num1, num2, num1 - num2);
 }
