ebaefa7135dd38512a8952508b993cca6f08cf6e
infi-null-0-local <119103652+Infi-null-0@users.noreply.github.com>
2022-11-27 20:49:49 +0530

    Update c_pogram_1.c
    added an update to multiply and divide

--- c_pogram_1.c
+++ c_pogram_1.c
@@ -5,5 +5,5 @@ void main()
     int num1, num2;
     printf("PROGRAM TO PRINT TWO NUMBERS\n\nEnter the two numbers :");
     scanf("%d%d", &num1, &num2);
-    printf("\nThe number 1 is : %d\nThe number 2 is : %d\n\nEND OF PROGRAM", num1, num2);
+    printf("\nThe number 1 is : %d\nThe number 2 is : %d\n\nTHE PRODUCT AND QUOTIENT ARE : %d %d\n\nEND OF PROGRAM", num1, num2, num1 * num2, num1 / num2);
 }
